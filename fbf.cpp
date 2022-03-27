c2f21f18c06cdb5dc75a713389e89c4b895bb834
CheeseDanish1 <65871181+CheeseDanish1@users.noreply.github.com>
2022-03-27 11:28:37 -0400

    Update fbf.cpp

--- fbf.cpp
+++ fbf.cpp
@@ -28,13 +28,19 @@ brain Brain;
 
 // Robot configuration code.
 controller Controller1 = controller(primary);
-motor Motor1 = motor(PORT1, ratio6_1, false);
-
-motor Motor2 = motor(PORT11, ratio6_1, false);
-
 motor Motor10 = motor(PORT10, ratio6_1, false);
 
+motor Motor19 = motor(PORT19, ratio6_1, false);
+
+motor Motor20 = motor(PORT20, ratio6_1, false);
+
 motor Motor9 = motor(PORT9, ratio18_1, false);
+
+motor Motor8 = motor(PORT8, ratio6_1, false);
+
+motor Motor4 = motor(PORT4, ratio18_1, false);
+
+motor Motor5 = motor(PORT5, ratio18_1, false);
 
 
 // define variable for remote controller enable/disable
@@ -51,41 +57,134 @@ bool RemoteControlCodeEnabled = true;
 using namespace vex;
 
 const int MOVING_MULTIPLIER = 6;
+const int clampVelocity = 75;
+bool band = false;
+// Motor 1 and 9 are the right motors and are ports 10 and 9
+// Motor 2 and 10 are the left motors and are port 19 and 20
+// Motor 3 is the spinny band and goes to port 11
 
-// Motor 1 is the right motor
-// Motor 2 is the left motor
-
-void start() {
+template<class Any>
+Any printBrain(Any val) { 
   Brain.Screen.clearLine(1);
   Brain.Screen.clearScreen();
-  Brain.Screen.print("Hewwo UwU <3");
+  Brain.Screen.print(val); 
+  return val; 
 }
 
-void MoveRightMotor() {
-  int pos = Controller1.Axis2.position();
+void MoveMotors(int pos, int pos2) {
   int a = ABS(pos)*MOVING_MULTIPLIER;
-  Motor2.setVelocity(a, rpm);
-  Motor2.spin(pos > 0 ? reverse : forward);
-  Motor10.setVelocity(a, rpm);
-  Motor10.spin(pos > 0 ? reverse : forward);
+  int a2 = ABS(pos2)*MOVING_MULTIPLIER;
+
+  Motor20.setVelocity(a, rpm);
+  Motor20.spin(pos < 0 ? forward : reverse);
+  Motor19.setVelocity(a, rpm);
+  Motor19.spin(pos < 0 ? forward : reverse);
+  Motor10.setVelocity(a2, rpm);
+  Motor10.spin(pos2 < 0 ? reverse : forward);
+  Motor9.setVelocity(a2, rpm);
+  Motor9.spin(pos2 < 0 ? reverse : forward);
 }
 
-void MoveLeftMotor() {
-  int pos = Controller1.Axis3.position();
-  int a = ABS(pos)*MOVING_MULTIPLIER;
-  Motor1.setVelocity(a, rpm);
-  Motor1.spin(pos > 0 ? forward : reverse);
-  Motor9.setVelocity(a, rpm);
-  Motor9.spin(pos > 0 ? forward : reverse);
+void assessMovement() {
+  // This is for turning (-100, 100)
+  // Corresponds to x axis
+  int pos1 = Controller1.Axis4.position();
+  // This is for speed(-100, 100)
+  // Corresponds to y axis
+  int pos2 = Controller1.Axis3.position();
+  
+  // Dont know why this works but im glad I found it
+  MoveMotors(pos2-pos1, pos2+pos1);
 }
 
-int whenStarted1() {
-  start();
-  Controller1.Axis3.changed(MoveLeftMotor);
-  Controller1.Axis2.changed(MoveRightMotor);
-  return 0;
+void clampDown() {
+  Motor4.setVelocity(clampVelocity, rpm);
+  Motor5.setVelocity(clampVelocity, rpm);
+  Motor4.spin(reverse);
+  Motor5.spin(forward);
+}
+
+void clampUp() {
+  Motor4.setVelocity(clampVelocity, rpm);
+  Motor5.setVelocity(clampVelocity, rpm);
+  Motor4.spin(forward );
+  Motor5.spin(reverse);
+}
+
+void clampStop() {
+Motor4.stop();
+Motor5.stop();
+}
+
+void startBand() {
+  Motor8.setVelocity(400, rpm);
+  Motor8.spin(forward);
+}
+
+void reverseBand() {
+  Motor8.setVelocity(400, rpm);
+  Motor8.spin(reverse);
+}
+
+void stopBand() {
+  Motor8.stop();
+}
+
+void initBand() {
+  Controller1.ButtonR1.pressed(startBand);
+  Controller1.ButtonR1.released(stopBand);
+  Controller1.ButtonR2.pressed(reverseBand);
+  Controller1.ButtonR2.released(stopBand);
+}
+
+// void startBand() {
+//   Motor8.setVelocity(400, rpm);
+//   band = !band;
+//   if (band) {
+//     Motor8.spin(reverse);
+//   } else {
+//     Motor8.stop();
+//   }
+// }
+
+// void reverseBand() {
+//   Motor8.setVelocity(400, rpm);
+//   Motor8.spin(reverse);
+// }
+
+// void stopBand() {
+//   Motor8.stop();
+// }
+
+// void initBand() {
+//   Controller1.ButtonR1.pressed(startBand);
+//   Controller1.ButtonR2.pressed(startBand);
+//   // Controller1.ButtonR2.pressed(reverseBand);
+// }
+
+void initClamp() {
+  Controller1.ButtonL2.pressed(clampDown);
+  Controller1.ButtonL2.released(clampStop);
+  Controller1.ButtonL1.pressed(clampUp);
+  Controller1.ButtonL1.released(clampStop);
+
+}
+
+void initStearing() {
+  Controller1.Axis3.changed(assessMovement);
+  Controller1.Axis4.changed(assessMovement);
+}
+
+void onStart() {
+  printBrain("Hewwo UwU :3");
+  Controller1.rumble(rumblePulse);
 }
 
 int main() {
-  whenStarted1();
+  Motor4.setStopping(brake);
+  Motor5.setStopping(brake);
+  onStart();
+  initBand();
+  initStearing();
+  initClamp();
 }
